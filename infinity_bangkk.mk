88156bf70f8bdecf97b50c68de0db4ca659f9358
Shoiya Akari <barryofc11@gmail.com>
2025-10-25 04:48:54 +0000

    bangkk: Infinity X Bringup!
    Signed-off-by: Shoiya Akari <barryofc11@gmail.com>

--- infinity_bangkk.mk
+++ infinity_bangkk.mk
@@ -11,10 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bangkk device
 $(call inherit-product, device/motorola/bangkk/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_bangkk
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := "akarizao"
+TARGET_SUPPORTS_BLUR := true
+TARGET_SHIPS_FULL_GAPPS := true
+TARGET_SHIPS_GOOGLE_DIALER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_HAS_UDFPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS := true
+
+PRODUCT_NAME := infinity_bangkk
 PRODUCT_DEVICE := bangkk
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
