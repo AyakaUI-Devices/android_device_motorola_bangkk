c13ccb5b8100e95c78d7e9c7db95abe586680b01
Shoiya Akari <barryofc11@gmail.com>
2025-11-25 00:12:50 +0000

    bangkk: Set Lineage Health control charging path This must now be set per device after migrating to soong_config_set. based on https://github.com/Motorola-SM6375-Devs/android_device_motorola_fogos/commit/273e990ec9f3d31696aca578b4d091b34db0f360
    Signed-off-by: Shoiya Akari <barryofc11@gmail.com>

--- device.mk
+++ device.mk
@@ -60,6 +60,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.mmi.overlay.rc
 
+# Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_current_limit)
+
 # LiveDisplay
 $(call soong_config_set,livedisplay_sysfs,enable_af,true)
 
