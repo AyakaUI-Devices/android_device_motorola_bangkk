537cf0b3c826c7d7b4dd7f5b84df02337eef88d2
Shoiya Akari <barryofc11@gmail.com>
2025-12-02 06:26:35 +0000

    Revert "bangkk: Introduce Dolby Atmos support" moved to common device
    This reverts commit f560b1804d791c842406145e132d57f16672273a.

--- device.mk
+++ device.mk
@@ -21,9 +21,6 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 # Inherit from motorola sm6375-common
 $(call inherit-product, device/motorola/sm6375-common/common.mk)
 
-# Dolby
-$(call inherit-product, hardware/dolby/dolby.mk)
-
 # GameBar
 PRODUCT_PACKAGES += \
 	Parts
@@ -43,8 +40,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-	$(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Fingerprint
 PRODUCT_PACKAGES += \
