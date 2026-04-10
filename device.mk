4bf7f679bc3da17c9864f0694f000c1980a4ae2e
ZedissP <juniordesigner077@gmail.com>
2026-04-10 14:03:43 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -110,8 +110,3 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/bangkk/bangkk-vendor.mk)
-
-# Improved ram management zram
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.zram.mark_idle_delay_mins=60 \
-    persist.sys.zram_enabled=true
