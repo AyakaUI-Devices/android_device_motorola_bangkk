359a8af1d62e164c9346246402aa05eb7d48f979
whyakari <barryofc11@gmail.com>
2026-08-22 01:59:31 -0300

    bangkk: Bringup AyakaUI
    Signed-off-by: whyakari <barryofc11@gmail.com>

--- ayaka_bangkk.mk
+++ ayaka_bangkk.mk
@@ -11,14 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bangkk device
 $(call inherit-product, device/motorola/bangkk/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AyakaUI stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_bangkk
+PRODUCT_NAME := ayaka_bangkk
 PRODUCT_DEVICE := bangkk
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g84 5G
+
+# Ayaka stuff.
+AYAKA_MAINTAINER := whyakari
+WITH_GMS := true
+IS_OFFICIAL := true
+WITH_KSU := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
